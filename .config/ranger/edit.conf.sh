c112b4712c3e6dd52ef6edeb11c764841513bf2d
JosefLitos <litosjos@fit.cvut.cz>
2023-12-31 00:08:19 +0100

    chore: custom springboot for bash - automated docs+optParsing+errors

--- .config/ranger/edit.conf.sh
+++ .config/ranger/edit.conf.sh
@@ -1,44 +1,33 @@
 # config for ../../bin/xdg-open
-toOpus() {
-	for f in "$@"; do
-		[[ $f =~ \.(flac|m4a|mp3|wav|wma)$ ]] || continue
-		# -t $(ffprobe "$f" -show_entries format=duration -v quiet -of csv="p=0" | awk '{print $1-2}') \
-		ffmpeg -v quiet -i "$f" -map 0:a -map_metadata -1 \
-			"${f%.*}.opus" && rm "$f"
-	done
-}
-try @toOpus .flac .m4a .mp3 .wav .wma
-stripMeta() {
-	for f in "$@"; do nosongmeta "$f" "/tmp/$f" && rm "$f" && mv "/tmp/$f" "$f"; done
-}
-try @stripMeta .mp4 .opus
-try @mp4tomkv .srt
-try @blockbench .json
-try @engrampa .jar
+# functions must be prefixed with @ otherwise they're run as external commands (and not found)
+# -t $(ffprobe "$f" -show_entries format=duration -v quiet -of csv="p=0" | awk '{print $1-2}') \
 try @gimp .png .jpg .jpeg .xcf
+try @engrampa .jar
 try @inkscape .svg
-# try @"kdenlive & dragon-drop -x -a" .mkv
+try @blockbench .json
 BLOCKING=1
+try 'mom -c' .flac .m4a .mp3 .wav .wma
+try 'mom --subtitles' .srt .mp4
+# try @"kdenlive & dragon-drop -x -a" .mkv
 editDir() {
 	# doesn't work if directories are inside the given one
 	CWD=$PWD
 	cd "$1"
 	f=(*)
 	if [[ $f =~ \.(jpg|heif|jpeg)$ ]]; then
-		# alternative: 
+		# uses perl-image-exiftool package
+		exiftool -d '%Y_%m_%d_%H%M%S.%%e' '-FileName<DateTimeOriginal' .
+		# alternative:
 		# fd -e jpg -e heif -x bash -c 'x="{}"
 		# file "$x" | grep datetime && mv "$x" "${x%/*}/$(file "$x" |
 		#   sed -En "s/.*datetime=(....):(..):(..) (..):(..):(..).*/\1_\2_\3_\4\5\6/p").${x//*.}"'
-		# uses perl-image-exiftool package
-		exiftool -d '%Y_%m_%d_%H%M%S.%%e' '-FileName<DateTimeOriginal' .
 		cd "$CWD"
 	elif [[ $f =~ \.(mp3|m4a|flac)$ ]]; then
 		toOpus "${f[@]}"
 		cd "$CWD"
 	else
 		cd "$CWD"
-		xterm ranger "$@"
+		pcmanfm "$@"
 	fi
 }
 EXPLORER=@editDir
-. ~/.config/open.conf.sh

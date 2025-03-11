294483e1c32874a298898851db115efac14aa6c9
JosefLitos <invisiblemancz@gmail.com>
2025-03-11 21:02:38 +0100

    fix(open)

--- .config/open.conf.sh
+++ .config/open.conf.sh
@@ -1,20 +1,24 @@
 #config for ../bin/xdg-open
-BROWSER=@$BROWSER
+BROWSER=@${BROWSER#@}
 TERM_BLOCKING=1
 try 'mpv --no-audio-display' +audio .m3u .m4a .mp3
-try @'mpv --no-terminal' +video
-try @twitch youtube.com/watch youtu.be
-try @geeqie .RAF
-if (($# > 1)); then
-	try @'swayimg -c list.all=no' +image
-else
-	try @swayimg +image
-fi
-# try @imv-dir +image
+try '@mpv --no-terminal' +video
+imgFallback() {
+	BLOCKING=1
+	# try @imv-dir +image
+	if (($# > 1)); then
+		try '@swayimg -c list.all=no' +image
+	else
+		try @swayimg +image
+	fi || try @geeqie +image
+}
+try @imgFallback +image
+try @engrampa .7z .bz2 .gz .rar .tar .tgz .xz .zip .zst
 try @zathura .pdf
 try @transmission-gtk magnet:
 try @thunderbird mailto:
-try @engrampa .7z .bz2 .gz .rar .tar .tgz .xz .zip .zst
+try @twitch youtube.com/watch youtu.be
 try 'java -jar' .jar
-try '' .AppImage
+try '' .AppImage # execute the file itself
+try "$BROWSER" http: https:
 FALLBACK=$EDITOR

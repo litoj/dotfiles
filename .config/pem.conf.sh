4af412ce3244aff87f635d2fa4fe55c196f1e956
litoj <invisiblemancz@gmail.com>
2026-02-02 01:04:13 +0059

    feat(pem): photo exif manager

---
+++ .config/pem.conf.sh
@@ -0,0 +1,22 @@
+executeConfig=(
+	--basename '2024-05'
+)
+
+categorizeConfig=(
+	--dst '/tmp/categorized'
+)
+
+fix35mmFL() {
+	bc <<<"1.5*$(getExif "$1" '$FocalLength')"
+}
+
+declare -gA editPresets=(
+	['fixManualFL']=editFixManualFL
+)
+editFixManualFL=(
+	-t FocalLength -v ''
+	-t FocalLengthIn35mmFormat -v '$fix35mmFL$'
+	-t LensInfo -v '$FocalLength $FocalLength undef undef'
+	-t MinFocalLength -v '$FocalLength'
+	-t MaxFocalLength -v '$FocalLength'
+)
--- .config/sway/move_ueberzug.sh
+++ .config/sway/move_ueberzug.sh
@@ -4,28 +4,28 @@
 tree=$(swaymsg -t get_tree -p)
 
 get_ws() {
-	echo "$tree" | grep -F "pid: $1" -B 5 |
-		sed -nE 's/.*workspace "([0-9].*)"$/\1/p' | tail -n 1
+	local ws=$(echo "$tree" | awk '$2=="workspace" {w=$3}; /pid: '$1'/ {print w;exit}')
+	echo "${ws:1:-1}"
 }
 
-get_win_id() { # ranger runs inside a terminal windowa or neovim
-	declare -i id=$1 lastid
-	while ((id != 1)); do
-		lastid=$id
-		id=$(ps -o ppid= "$lastid")
+get_parent_win_ws() { # ranger runs inside a terminal windowa or neovim
+	declare -i id=$1
+	local ws
+	while [[ -z $ws ]] ; do
+		id=$(ps -o ppid= "$id")
+		ws=$(get_ws "$id")
 	done
-	echo "$lastid"
+	echo "$ws"
 }
 
 declare -i corrected=0
 while read -r pid; do
 	uw=$(get_ws $pid)
 	[[ $uw ]] || continue
-	ranger_win=$(get_win_id $pid)
-	rw=$(get_ws $ranger_win)
+	rw=$(get_parent_win_ws $pid)
 	[[ $uw == $rw ]] && continue # ignore correctly placed ueberzug windows
 
-	((corrected+=1))
+	((corrected += 1))
 	swaymsg "[pid=$pid] move workspace $rw" &>/dev/null
 done < <(ps -o pid= -C ueberzug)
 
